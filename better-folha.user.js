74b91f4575dba3623696b0b52d6a86e83cdfac7a
Bruno França dos Reis <bfreis@gmail.com>
2014-10-19 04:55:52 -0200

    fix namespace

--- better-folha.user.js
+++ better-folha.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Better Folha
-// @namespace    http://github.com/bfreis
+// @namespace    http://github.com/bfreis/better-folha
 // @version      0.1
 // @description  Gives back the visitor of Folha his rights to use his computer as he sees appropriate.
 // @author       Bruno Reis
