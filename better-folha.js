bc61a8a5eefa26fc27b368a971a2e7b711042091
Bruno França dos Reis <bfreis@gmail.com>
2014-10-19 04:44:47 -0200

    initial version

---
+++ better-folha.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Better Folha
+// @namespace    http://github.com/bfreis
+// @version      0.1
+// @description  Gives back the visitor of Folha his rights to use his computer as he sees appropriate.
+// @author       Bruno Reis
+// @match        http://*.folha.uol.com.br/*
+// @grant        none
+// ==/UserScript==
+
+$("body").unbind("copy");
